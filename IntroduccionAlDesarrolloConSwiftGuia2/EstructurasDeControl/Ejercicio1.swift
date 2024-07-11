25b7444d6bf4c3dc461bca2f7791fd271a445c28
orgb95 <orgb10917@gmail.com>
2024-07-10 19:50:21 -0600

    Ejercicio1 Estructuras de control terminado

--- IntroduccionAlDesarrolloConSwiftGuia2/EstructurasDeControl/Ejercicio1.swift
+++ IntroduccionAlDesarrolloConSwiftGuia2/EstructurasDeControl/Ejercicio1.swift
@@ -6,3 +6,25 @@ array de enteros y lo ordene utilizando el algoritmo de ordenamiento por selecci
 Implementa la lógica del algoritmo y prueba la función con un array de ejemplo.
 
 */
+
+func ordenarPorSeleccion(ar: inout [Int]) {
+    let n = ar.count
+    for i in 0..<n-1 {
+        var minIndice = i
+        
+        for j in i+1..<n {
+            if ar[j] < ar[minIndice] {
+                minIndice = j
+            }
+        }
+        
+        if minIndice != i {
+            ar.swapAt(i, minIndice)
+        }
+    }
+}
+
+var numeros = [64, 25, 12, 22, 11]
+print("Array original: \(numeros)")
+ordenarPorSeleccion(ar: &numeros)
+print("Array ordenado: \(numeros)")
